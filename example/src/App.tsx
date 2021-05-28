0adceae7b06955adc6a22e9896db6b02669b5e67
samassango <samassango@gmail.com>
2021-05-28 17:17:00 +0200

    Implementing the multisteps component with styles attributes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,15 +4,39 @@ import { StyleSheet, View, Text } from 'react-native';
 import MultiSteps from 'react-native-multi-steps';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    MultiSteps.multiply(3, 7).then(setResult);
-  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <MultiSteps
+        onMoveNext={function (data: any): void { console.log("next", data) }}
+        onMovePrevious={function (data: any): void { console.log("previous", data) }}
+        onSubmit={function () { console.log('Submit') }}>
+        <View>
+          <Text>
+            A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument.
+           </Text>
+        </View>
+        <View>
+          <Text>
+            Piedmont, or mountain, glaciers are found in many parts of the world. In North America they are distributed along the mountain ranges of the Pacific Coast from central California northward. They abound in the Andes range in South America and are familiar and greatly admired spectacles in the Alps, the Pyrenees, the Caucasus Mountains and the mountains of Scandanavia. Rivers of ice flow down the valleys of various Asian mountain ranges, including the Himalayas, the Hindu Kush, and the Karakoram and Kunlun ranges. They are also a feature of the Southern Alps of New Zealand and are found in the lofty mountains of New Guinea. The largest piedmont glaciers are the Malaspina and Bering glaciers, both in Alaska. </Text>
+        </View>
+        <View>
+          <Text>
+            Scientists' research has revealed that viruses are by far the most abundant life forms on Earth. There are a million times more viruses on the planet than stars in the universe. Viruses also harbor the majority of genetic diversity on Earth. Scientists are finding evidence of viruses as a planetary force, influencing the global climate and geochemical cycles. They have also profoundly shaped the evolution of their hosts. The human genome, for example, contains 100,000 segments of virus DNA.
+          </Text>
+        </View>
+        <View>
+          <Text>
+            sibusiso love you
+          </Text>
+        </View>
+        <View>
+          <Text>
+            Scientists' research has revealed that viruses are by far the most abundant life forms on Earth. There are a million times more viruses on the planet than stars in the universe. Viruses also harbor the majority of genetic diversity on Earth. Scientists are finding evidence of viruses as a planetary force, influencing the global climate and geochemical cycles. They have also profoundly shaped the evolution of their hosts. The human genome, for example, contains 100,000 segments of virus DNA.
+          </Text>
+        </View>
+      </MultiSteps>
+
     </View>
   );
 }
@@ -22,10 +46,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,134 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+
+import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types";
+import { StyleSheet, View, ActivityIndicator, Text, SafeAreaView, TouchableOpacity } from "react-native";
+
+function RNMultiStep(props: any): any {
+  const [stepIndex, setStepIndex] = useState(0);
+  const [Component, setComponent] = useState(null);
+  const [canMove, setCanMove] = useState({ canMovePrevious: false, canMoveNext: true });
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  useEffect(function () {
+    setComponent(props.children[stepIndex]);
+  }, [stepIndex]);
+
+  useEffect(function () {
+    setIsSubmit(isSubmit);
+  }, [props.onSubmit])
+
+  function onNext(): void {
+    const step = stepIndex + 1;
+    const len = props.children.length - 1;
+    let isNextValid = step === len ? false : true;
+    let isPrevious = step > 0 ? true : false;
+    if (!!props.children[step]) {
+      setCanMove({
+        canMoveNext: isNextValid,
+        canMovePrevious: isPrevious
+      });
+      setStepIndex(step);
+    }
+    props.onMoveNext(canMove);
+  }
+
+  function onPrevious(): void {
+    if (stepIndex !== 0) {
+      const step = stepIndex - 1;
+
+      let isPreviousValid = step === 0 ? false : true;
+      let isNext = step === props.children.length - 1 ? false : true;
+      setCanMove({
+        canMoveNext: isNext,
+        canMovePrevious: isPreviousValid
+      });
+      setStepIndex(step);
+    }
+    props.onMovePrevious(canMove);
+  }
+  function onSubmit(): void {
+    props.onSubmit();
+  }
+  return (
+    <SafeAreaView style={props.containerStyle ? props.containerStyle : styles.container}>
+      {Component !== null ? Component :
+        <View>
+          <ActivityIndicator size="large" />
+          <Text>Loading...</Text>
+        </View>}
+      <View style={props.containerButtonStyle ? props.containerButtonStyle : styles.fixToText}>
+        {canMove.canMovePrevious &&
+          <TouchableOpacity
+            style={props.buttonStyle ? props.buttonStyle : styles.styleBtn}
+            onPress={onPrevious}
+          >
+            <Text style={props.buttonLabelStyle ? props.buttonLabelStyle : styles.styleBtnLabel}>Previous</Text>
+          </TouchableOpacity>
+        }
+
+        {canMove.canMoveNext &&
+          <TouchableOpacity
+            style={props.buttonStyle ? props.buttonStyle : styles.styleBtn}
+            onPress={onNext}
+          >
+            <Text style={props.buttonLabelStyle ? props.buttonLabelStyle : styles.styleBtnLabel}>Next</Text>
+          </TouchableOpacity>
+        }
+        {!canMove.canMoveNext &&
+          typeof props.onSubmit === 'function' &&
+          <TouchableOpacity
+            style={props.buttonStyle ? props.buttonStyle : styles.styleBtn}
+            onPress={onSubmit}
+          >
+            <Text style={props.buttonLabelStyle ? props.buttonLabelStyle : styles.styleBtnLabel}>Submit</Text>
+          </TouchableOpacity>
+        }
+      </View>
+    </SafeAreaView >
+  );
+
+
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+    width: '100%',
+    marginVertical: 16,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  styleBtn: {
+    alignItems: "center",
+    backgroundColor: "#1e1ee3",
+    borderRadius: 50,
+    shadowColor: '#5252d1',
+    margin: 10,
+    padding: 20,
+    width: '40%'
+  },
+  styleBtnLabel: {
+    color: '#f7f7ff',
+    fontWeight: 'bold',
+  },
+});
+RNMultiStep.propTypes = {
+  children: PropTypes.any,
+  containerStyle: PropTypes.object,
+  containerButtonStyle: PropTypes.object,
+  buttonStyle: PropTypes.object,
+  buttonLabelStyle: PropTypes.object,
+  onMoveNext: PropTypes.func,
+  onMovePrevious: PropTypes.func,
+  onSubmit: PropTypes.func
+};
+export default RNMultiStep;
